fecfd922accc81c4236cc73670329f99a327f40e
caluapps <office@calu-apps.at>
2018-02-21 22:14:43 +0100

    JSX Expressions; Conditional Rendering; const and let; Arrow Functions;

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,107 @@
 
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
-var template = React.createElement(
-  'h1',
+/* Challenge: create app object title/subtitle
+ * use title/subtitle in the template
+ * render template
+ */
+
+/* Challenge: only render the subtitle (and p tag) if subtitle exists &&
+ * render new p tag - if options.length > 0
+ *  if true: 'here are your options'
+ *  if false: 'no options'
+ */
+
+var app = {
+  title: 'Title',
+  subtitle: 'Some subtitles',
+  options: ['One', 'Two']
+
+  // JSX - JavaScript XML
+};var template = React.createElement(
+  'div',
   null,
-  'Indecision'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options!' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+
+var user = {
+  name: 'j:caluapps',
+  do: 'App Development',
+  for: 5,
+  where: 'Vienna'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Do: ',
+    user.do
+  ),
+  user.for && user.for >= 2 && React.createElement(
+    'p',
+    null,
+    'For: ',
+    user.for,
+    ' years'
+  ),
+  getLocation(user.where)
+);
+
+/* Challenge: Create a templateTwo var JSX expression
+ *  div
+ *    h1 -> j:caluapps
+ *    p -> Age: 32
+ *    p -> Location: Vienna
+ * Render templateTwo instead of template
+*/
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,65 @@
 console.log('App.js is running!');
 
+/* Challenge: create app object title/subtitle
+ * use title/subtitle in the template
+ * render template
+ */
+
+/* Challenge: only render the subtitle (and p tag) if subtitle exists &&
+ * render new p tag - if options.length > 0
+ *  if true: 'here are your options'
+ *  if false: 'no options'
+ */
+
+const app = {
+  title: 'Title',
+  subtitle: 'Some subtitles',
+  options: ['One', 'Two']
+}
+
 // JSX - JavaScript XML
-var template = <h1>Indecision</h1>;
-var appRoot = document.getElementById('app');
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options!' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+const user = {
+  name: 'j:caluapps',
+  do: 'App Development',
+  for: 5,
+  where: 'Vienna'
+}
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+const templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    <p>Do: {user.do}</p>
+    {(user.for && user.for >= 2) && <p>For: {user.for} years</p>}
+    {getLocation(user.where)}
+  </div>
+);
+
+/* Challenge: Create a templateTwo var JSX expression
+ *  div
+ *    h1 -> j:caluapps
+ *    p -> Age: 32
+ *    p -> Location: Vienna
+ * Render templateTwo instead of template
+*/
+
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
