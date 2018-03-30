b5639e81b451ffe96f4876d490e5a7e768efb8fb
caluapps <office@calu-apps.at>
2018-03-30 16:47:26 +0200

    adding state to counter app II;

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,6 +19,9 @@ var Counter = function (_React$Component) {
     _this.handleAddOne = _this.handleAddOne.bind(_this);
     _this.handleMinusOne = _this.handleMinusOne.bind(_this);
     _this.handleReset = _this.handleReset.bind(_this);
+    _this.state = {
+      count: 0
+    };
     return _this;
   }
 
@@ -26,16 +29,31 @@ var Counter = function (_React$Component) {
     key: 'handleAddOne',
     value: function handleAddOne() {
       console.log('Add one');
+      this.setState(function (prevState) {
+        return {
+          count: prevState.count + 1
+        };
+      });
     }
   }, {
     key: 'handleMinusOne',
     value: function handleMinusOne() {
       console.log('Minus One');
+      this.setState(function (prevState) {
+        return {
+          count: prevState.count - 1
+        };
+      });
     }
   }, {
     key: 'handleReset',
     value: function handleReset() {
       console.log('reset');
+      this.setState(function () {
+        return {
+          count: 0
+        };
+      });
     }
   }, {
     key: 'render',
@@ -46,7 +64,8 @@ var Counter = function (_React$Component) {
         React.createElement(
           'h1',
           null,
-          'Count: '
+          'Count: ',
+          this.state.count
         ),
         React.createElement(
           'button',
