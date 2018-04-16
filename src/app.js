1919cbc340b671dc3f4384376a29c6bf6a19b921
caluapps <office@calu-apps.at>
2018-04-16 23:40:56 +0200

    Passing Children to Component;

--- src/app.js
+++ src/app.js
@@ -2,35 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp.js';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+// ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
-class OldSyntax {
-  constructor() {
-    this.name = 'Mike';
-    this.getGreeting = this.getGreeting.bind(this);
-  }
-  getGreeting() {
-    return `Hi. My name is ${this.name}.`;
-  }
+/* Pass Data to a Component
+const Layout = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      {props.content}
+      <p>footer</p>
+    </div>
+  );
 }
 
-const oldSyntax = new OldSyntax();
-console.log(oldSyntax.getGreeting());
+const template = (
+  <div>
+    <h1>Page Title</h1>
+    <p>This is my page</p>
+  </div>
+);
 
-const getGreeting = oldSyntax.getGreeting;
-console.log(getGreeting());
+ReactDOM.render(<Layout content={template}/>, document.getElementById('app')); */
 
-// -----------------
-
-class NewSyntax {
-  name = 'Jen';
-  getGreeting = () => {
-    return `Hi. My name is ${this.name}.`;
-  }
+// Pass Children to Component - Children Prop
+const Layout = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      {props.children}
+      <p>footer</p>
+    </div>
+  );
 }
 
-const newSyntax = new NewSyntax();
-console.log(NewSyntax);
-
-const newGetGreeting = newSyntax.getGreeting;
-console.log(newGetGreeting());
+ReactDOM.render((
+    <Layout>
+      <div>
+        <h1>Page Title</h1>
+        <p>This is my page</p>
+      </div>
+    </Layout>
+  ),
+  document.getElementById('app'));
